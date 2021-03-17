335fec24baddd9960f5a02399d11a126430f0f66
Nathalie Bitzer <68108453+HelloBitzer@users.noreply.github.com>
2021-03-17 14:53:52 +0100

    Dtos

--- WPF/MainWindow.xaml.cs
+++ WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MyNamespace;
 
 namespace WPF
 {
@@ -20,10 +22,61 @@ namespace WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Kundenliste erstellen
+        //private static string url = @"http://localhost:44328";
+        //public ObservableCollection<People> kundenListe { get; set; } = new ObservableCollection<People>();
+
+        //Terminliste erstellen
+        //public ObservableCollection<Termins> terminListe { get; set; } = new ObservableCollection<Termins>();
+
+        //private Client client = null;
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //client = new Client(url);
+
+            ////Kundendaten laden 
+
+            //kundenListe = new ObservableCollection<People>(LoadPeoples());
+
+            //terminListe = new ObservableCollection<Termins>(LoadTermins());
+
+            ////Datengrid an Listen binden
+            //DG_Kunden.ItemsSource = kundenListe;
+            //DG_Termine.ItemsSource = terminListe;
         }
+
+
+        //private ICollection<People> LoadPeoples()
+        //{
+        //    try
+        //    {
+        //        return client.GetPeopleAllAsync().Result;
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        MessageBox.Show($"Die Kundenliste konnte nicht geladen werden {e.Message}");
+        //        return null;
+        //    }
+        //}
+
+        //private ICollection<Termins> LoadTermins()
+        //{
+        //    try
+        //    {
+        //        return client.GetTerminsAllAsync().Result;
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        MessageBox.Show($"Die Terminliste konnte nicht geladen werden {e.Message}");
+        //        return null;
+        //    }
+        //}
+
 
         private void BT_KD_Anlegen_OnClick(object sender, RoutedEventArgs e)
         {
